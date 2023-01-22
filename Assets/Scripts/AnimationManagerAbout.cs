dfd84220bc081951a7157105d4c662f7916b14c9
AkionGarro <carloscamp1008@estudianTEC.cr>
2023-01-22 17:25:33 -0600

    Complete About us

--- Assets/Scripts/AnimationManagerAbout.cs
+++ Assets/Scripts/AnimationManagerAbout.cs
@@ -6,22 +6,26 @@ public class AnimationManagerAbout : MonoBehaviour
 {
 
     public Animator animator1;
+    public AudioSource audioSource;
 
     public IEnumerator loadAkionAnimation()
     {
         animator1.SetBool("Enter", true);
+        audioSource.Play();
         yield return new WaitForSeconds(1.5f);
         animator1.SetBool("Enter", false);
     }
     public IEnumerator loadBryamAnimation()
     { 
-        animator1.SetBool("Enter", true);  
+        animator1.SetBool("Enter", true); 
+        audioSource.Play(); 
         yield return new WaitForSeconds(1.5f);
         animator1.SetBool("Enter", false);
     }
     public IEnumerator loadReynerAnimation()
     {
         animator1.SetBool("Enter", true);
+        audioSource.Play();
         yield return new WaitForSeconds(1.5f);
         animator1.SetBool("Enter", false);
 
